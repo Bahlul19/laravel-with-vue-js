a9844c4fa76dc3c61a88490f7667b3c67baada63
Bahlul Siddiquee <bahlul.tausif@gmail.com>
2024-04-16 13:27:42 +0600

    templating done as per project

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -3,21 +3,13 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Data Syncing</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        Hello 2025.
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
-<script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
-</script>
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
@@ -22,6 +23,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
+            '@': '/resources/js',
             vue: 'vue/dist/vue.esm-bundler.js',
         },
     },
